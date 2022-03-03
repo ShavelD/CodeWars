deeca6f7bd0dd64dfccfa4af428c2fe7c31234c0
dima shav <legrand.97@mail.ru>
2022-03-03 16:16:28 +0300

    PEP

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Very simple, given an integer or a floating-point number, find its opposite.
 
-10; -->   -10
-14; -->   -14
+10; --> -10;
+14; --> -14;
 
 function opposite(number) {
-    return(-number);
-  }
+    return (-number);
+}
