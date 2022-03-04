73b8f7272e48dab5c5a678ed717f7e55aaa7cfbe
dima shav <legrand.97@mail.ru>
2022-03-04 13:53:02 +0300

    PEP

--- script.js
+++ script.js
@@ -6,3 +6,17 @@
 function opposite(number) {
     return (-number);
 }
+
+//Given an array of integers your solution should find the smallest integer.
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        let smallest = args[0]
+
+        for (let i = 0; i < args.length; i++) {
+            if (args[i] < smallest) smallest = args[i]
+        }
+
+        return smallest
+    }
+}
