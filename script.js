1d3016902d9fc7b1514c690ab53e3621df0822f6
dima shav <legrand.97@mail.ru>
2022-03-04 14:17:50 +0300

    PEP

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ class SmallestIntegerFinder {
         return smallest
     }
 }
+
+/*Given a set of numbers, return the additive inverse of each. Each positive 
+becomes negatives, and the negatives become positives.*/
+
+function invert(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 0) {
+            return 0
+        } else {
+            array[i] = array[i] * -1
+            return array;
+        }
+    }
+}
+
+console.log(invert([1, -2, 3, 0]))
