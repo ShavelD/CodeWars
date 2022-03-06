60574ca89ea81eef779d795ad43566953f6a113a
dima shav <legrand.97@mail.ru>
2022-03-06 22:55:35 +0300

    PEP

--- script.js
+++ script.js
@@ -157,6 +157,12 @@ You get given the time in hours and you need to return,
 // wait is the number of people waiting to get on to the bus excluding the driver.
 // If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 
-function enough(cap, on, wait) {
-    return Math.max(on + wait - cap, 0);
-  }
+// function enough(cap, on, wait) {
+//     return Math.max(on + wait - cap, 0);
+//   }
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// function doubleInteger(i) {
+//     return i * 2;
+// }
