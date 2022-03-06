e66e4f98f4623954ab6f0f7bdbb95fc83e9dee29
dima shav <legrand.97@mail.ru>
2022-03-06 20:52:10 +0300

    PEP

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 //Very simple, given an integer or a floating-point number, find its opposite.
 
-10; --> -10;
-14; --> -14;
+// 10; --> -10;
+// 14; --> -14;
 
-function opposite(number) {
-    return (-number);
-}
+// function opposite(number) {
+//     return (-number);
+// }
 
 //Given an array of integers your solution should find the smallest integer.
 
-class SmallestIntegerFinder {
-    findSmallestInt(args) {
-        let smallest = args[0]
+// class SmallestIntegerFinder {
+//     findSmallestInt(args) {
+//         let smallest = args[0]
 
-        for (let i = 0; i < args.length; i++) {
-            if (args[i] < smallest) smallest = args[i]
-        }
+//         for (let i = 0; i < args.length; i++) {
+//             if (args[i] < smallest) smallest = args[i]
+//         }
 
-        return smallest
-    }
-}
+//         return smallest
+//     }
+// }
 
 /*Given a set of numbers, return the additive inverse of each. Each positive 
 becomes negatives, and the negatives become positives.*/
 
-function invert(array) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === 0) {
-            return 0
-        } else {
-            array[i] = array[i] * -1
-            return array;
-        }
-    }
-}
+// function invert(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === 0) {
+//             return 0
+//         } else {
+//             array[i] = array[i] * -1
+//             return array;
+//         }
+//     }
+// }
 
-console.log(invert([1, -2, 3, 0]))
+// console.log(invert([1, -2, 3, 0]))
 
 
 /*Create a function which answers the question "Are you playing banjo?".
 If your name starts with the letter "R" or lower case "r", you are playing banjo!*/
 
-function areYouPlayingBanjo(name) {
-    for (let i = 0; i < name.length; i++) {
-        if (name[i] === 'R' || name[i] === 'r') {
-            return name + " plays banjo"
-        } else {
-            return name + " does not play banjo"
-        }
-    }
-}
+// function areYouPlayingBanjo(name) {
+//     for (let i = 0; i < name.length; i++) {
+//         if (name[i] === 'R' || name[i] === 'r') {
+//             return name + " plays banjo"
+//         } else {
+//             return name + " does not play banjo"
+//         }
+//     }
+// }
 
 /*Nathan loves cycling.
 Because Nathan knows it is important to stay hydrated, he drinks 0.5,
@@ -57,9 +57,9 @@ Because Nathan knows it is important to stay hydrated, he drinks 0.5,
 You get given the time in hours and you need to return,
  the number of litres Nathan will drink, rounded to the smallest value.*/
 
-function litres(time) {
-    return Math.floor(time / 2);
-}
+// function litres(time) {
+//     return Math.floor(time / 2);
+// }
 
 // Trolls are attacking your comment section!
 // A common way to deal with this situation is to remove all,
@@ -70,10 +70,10 @@ function litres(time) {
 //  become "Ths wbst s fr lsrs LL!".
 // Note: for this kata y isn't considered a vowel.
 
-function disemvowel(inputString) {
-    var outputString = inputString.replace(/[aeiouAEIOU]/g, "");
-    return outputString;
-}
+// function disemvowel(inputString) {
+//     var outputString = inputString.replace(/[aeiouAEIOU]/g, "");
+//     return outputString;
+// }
 
 // Sentence Smash
 // Write a function that takes an array of words and smashes them together,
@@ -82,44 +82,64 @@ function disemvowel(inputString) {
 // spaces between each word. Be careful, there shouldn't be a space at the,
 // beginning or the end of the sentence!
 
-function smash(words) {
-    return words.join(" ");
-};
+// function smash(words) {
+//     return words.join(" ");
+// };
 
 // Given a string, you have to return a string in which each character,
 // (case-sensitive) is repeated once.
 
-function doubleChar(str) {
-    var newString = "";
-    for (i = 0; i < str.length; i++) {
-        newString += str[i] + str[i];
-    }
-    return newString;
-}
+// function doubleChar(str) {
+//     var newString = "";
+//     for (i = 0; i < str.length; i++) {
+//         newString += str[i] + str[i];
+//     }
+//     return newString;
+// }
 
 // Make a program that filters a list of strings and returns,
 // a list with only your friends name in it.
 // If a name has exactly 4 letters in it, 
 // you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not.
 
-function friend(friends) {
-    return friends.filter(el => el.length === 4);
-}
+// function friend(friends) {
+//     return friends.filter(el => el.length === 4);
+// }
 
 // Grade book
 // Complete the function so that it finds the average of the,
 // three scores passed to it and returns the letter value associated with that grade.
 
-function getGrade(s1, s2, s3) {
-    const score = (s1 + s2 + s3) / 3;
-    if (score >= 90) {
-        return 'A';
-    } else if (score >= 80) {
-        return 'B';
-    } else if (score >= 70) {
-        return 'C';
-    } else if (score >= 60) {
-        return 'D';
-    } else {
-        return 'F';
-    }
+// function getGrade(s1, s2, s3) {
+//     const score = (s1 + s2 + s3) / 3;
+//     if (score >= 90) {
+//         return 'A';
+//     } else if (score >= 80) {
+//         return 'B';
+//     } else if (score >= 70) {
+//         return 'C';
+//     } else if (score >= 60) {
+//         return 'D';
+//     } else {
+//         return 'F';
+//     }
+
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// he numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// let number = function (array) {
+//     const resultArr = [];
+
+//     for (let i = 0, j = 1; i < array.length; i += 1) {
+//         if (array.length === 0) {
+//             return `Empty array should return empty array`;
+//         } else {
+//             resultArr.push(`${j}: ${array[i]}`);
+//             j += 1;
+//         }
+//     }
+//     return resultArr;
+// }
+
