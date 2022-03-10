a0e2847ea2dbd86c182ab9475ef65dfddac3f9e5
dima shav <legrand.97@mail.ru>
2022-03-10 15:20:35 +0300

    PEP

--- script.js
+++ script.js
@@ -212,3 +212,16 @@ You get given the time in hours and you need to return,
 // function stringToArray(string) {
 //     return string.trim().split(" ");
 // }
+
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+// function findShort(s) {
+//     let res = s.split(' ');
+//     let a = Infinity;
+
+//     for (let i = 0; i < res.length; i++) {
+//         a = Math.min(res[i].length, a);
+//     }
+//     return a;
+// }
