ee0b0b70970a73baac0738221a5fcd7d60f79925
ShavelD <legrand.97@mail.ru>
2022-04-03 22:26:53 +0300

    PEP

--- script.js
+++ script.js
@@ -357,3 +357,23 @@ You get given the time in hours and you need to return,
 //         return total - cap;
 //     }
 // }
+
+// You will be given an array of numbers.You have to sort the odd numbers
+//     in ascending order while leaving the even numbers at their original positions.
+
+// function sortArray(array) {
+//     // Return a sorted array.
+//     const even = array.map((elm) => elm % 2 === 0 ? elm : "");
+//     const odd = array.filter((elm) => elm % 2 !== 0).sort((a, b) => a - b);
+//     const result = [];
+
+//     for (let i = 0, j = 0; i < array.length; i += 1) {
+//         if (even[i] === "") {
+//             result.push(odd[+j]);
+//             j += 1;
+//         } else {
+//             result.push(even[i]);
+//         }
+//     }
+
+//     return result;
