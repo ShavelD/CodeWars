40099b67df22f8bc0e6cc8b93f34869c296281d0
ShavelD <legrand.97@mail.ru>
2022-04-03 22:21:19 +0300

    PEP

--- script.js
+++ script.js
@@ -342,3 +342,18 @@ You get given the time in hours and you need to return,
 //     }
 //     return arr
 // }
+
+// The Story:
+// Bob is working as a bus driver.However, he has become extremely popular amongst,
+//     the city's residents. With so many passengers wanting to get aboard his bus, he sometimes,
+// has to face the problem of not enough space left 
+// on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+// function enough(cap, on, wait) {
+//     let total = on + wait;
+//     if (total <= cap) {
+//         return 0;
+//     } else {
+//         return total - cap;
+//     }
+// }
