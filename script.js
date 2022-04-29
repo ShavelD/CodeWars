47a24248afb46e3081dafc8181b84cdbccea1d89
ShavelD <legrand.97@mail.ru>
2022-04-29 15:08:22 +0300

    PEP

--- script.js
+++ script.js
@@ -488,3 +488,20 @@ You get given the time in hours and you need to return,
 //         return value1 / value2
 //     }
 // }
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+Implement a function that determines whether
+a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+function isIsogram(str){
+    var i, j;
+    str = str.toLowerCase();
+    for(i = 0; i < str.length; ++i) {
+      for(j = i + 1; j < str.length; ++j) {
+        if(str[i] === str[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+ }
