e64559847c7115f5b55e811320f81e57e7a3ce2b
ShavelD <legrand.97@mail.ru>
2022-05-05 10:45:40 +0300

    PEP

--- script.js
+++ script.js
@@ -519,3 +519,13 @@ You get given the time in hours and you need to return,
 //     return a + GetSum(a-1, b)
 // };
 // }
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// function even_or_odd(number) {
+//         if (number % 2 === 0) {
+//             return 'Even';
+//         } else {
+//             return 'Odd';
+//         }
+//     }
