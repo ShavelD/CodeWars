bebf2b77bc48c02320bdb7083aaab6e7122e2a21
ShavelD <legrand.97@mail.ru>
2022-05-23 23:59:18 +0300

    PEP

--- script.js
+++ script.js
@@ -570,3 +570,12 @@ You get given the time in hours and you need to return,
 // function numberToString(num) {
 //     return  num.toString();
 // }
+
+// You will be given an array a and a value x. All you need to do is,
+// check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+// function check(a, x) {
+//  return a.includes(x) ? true : false;
+// }
