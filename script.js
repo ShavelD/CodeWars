07f471c1e29bf33375853ef4b4f1c4fbd96b8fda
ShavelD <legrand.97@mail.ru>
2022-06-02 13:54:43 +0300

    RE

--- script.js
+++ script.js
@@ -644,3 +644,14 @@ You get given the time in hours and you need to return,
 
 //     return n.toString().split('').reverse().map(Number);
 // }
+
+// The cockroach is one of the fastest insects. 
+// Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer(= floored).
+
+// For example:
+
+// 1.08 -- > 30
+
+// function cockroachSpeed(s) {
+//     return Math.floor(s * 100000 / 3600);
+// }
