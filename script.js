b0b24d90b34a7c03648bf790f427a8c2d42b9105
ShavelD <legrand.97@mail.ru>
2022-07-25 17:09:13 +0300

    pep

--- script.js
+++ script.js
@@ -701,3 +701,20 @@ You get given the time in hours and you need to return,
     
 //     return totalCost
 // }
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+// function greet (name, owner) {
+//     if (name == owner) {
+//         return("Hello boss")
+//     }
+//     else {
+//         return("Hello guest")
+//     }
+// }
