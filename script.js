e0ad571e6bd1b5ac19e0f21a913bdccfa44d6ea9
ShavelD <legrand.97@mail.ru>
2022-07-26 22:52:40 +0300

    Codewars

--- script.js
+++ script.js
@@ -718,3 +718,15 @@ You get given the time in hours and you need to return,
 //         return("Hello guest")
 //     }
 // }
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// function century(year) {
+//     // Finish this :)
+//     let bearGrills = Math.ceil(year / 100)
+//     return bearGrills;
+//   }
