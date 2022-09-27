43d483b6e692866696342b5a6546ff5c17c09ddc
ShavelD <legrand.97@mail.ru>
2022-09-27 15:49:04 +0300

    POP

--- script.js
+++ script.js
@@ -914,5 +914,21 @@ You get given the time in hours and you need to return,
 // function isPalindrome(str) {
 //     str = str.toLowerCase()   
 //       return str === str.split('').reverse().join('')
-      
+
 //     }
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum(also as a string):
+
+// Example: (Input1, Input2-- > Output)
+
+// "4", "5" -- > "9"
+// "34", "5" -- > "39"
+// "", "" -- > "0"
+// "2", "" -- > "2"
+// "-5", "3" -- > "-2"
+
+// function sumStr(a, b) {
+//     let z = Number(a) + Number(b);
+//     let sumStr = z.toString();
+//     return sumStr
+// }
